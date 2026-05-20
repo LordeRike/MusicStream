c0cc0f27b31796b7ea11939bfb372930e9eedac0
Henrique <90357661+LordeRike@users.noreply.github.com>
2026-05-19 21:37:54 -0300

    Update Musica.java

--- Musica.java
+++ Musica.java
@@ -1,3 +1,10 @@
 public class Musica {
+    String nomeMusica;
+    int faixa;
+
+    public void exibirDados(String nomeMusica, int faixa) {
+        System.in.println("Nome: "+nomeMusica);
+        System.in.println("Faixa: "+faica);
+    }
     
 }
